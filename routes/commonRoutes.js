595e6426f6e6cd1b7d0c1b8c5910c48d03053f5d
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2024-12-01 19:05:36 +0300

    deploy

--- routes/commonRoutes.js
+++ routes/commonRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const commonController = require('../controllers/commonController');
 
-router.post('/validate-link', commonController.validateLink);
+router.get('/validate-link', commonController.validateLink);
 
 
 module.exports = router;
