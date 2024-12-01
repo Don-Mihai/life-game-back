64a8d260a2528b560720cd973fbf36e7e168c9e6
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2024-12-01 19:00:38 +0300

    deploy

--- controllers/commonController.js
+++ controllers/commonController.js
@@ -1,17 +1,21 @@
 // Получение навыков пользователя
 const axios = require("axios");
 exports.validateLink = async (req, res) => {
-    const { url } = req.body;
+    const { url } = req.query; // Получаем параметр "url" из строки запроса
+
+    if (!url || typeof url !== 'string') {
+        return res.status(400).json({ success: 0, message: 'URL не передан или некорректен' });
+    }
 
     try {
         // Пытаемся запросить URL
-        const response = await axios.get(url, { timeout: 5000 });
+        const response = await axios.get(decodeURIComponent(url), { timeout: 5000 });
 
         if (response.status === 200) {
-            // Если ссылка доступна, возвращаем валидность
+            // Если ссылка доступна, возвращаем успешный результат
             return res.json({
                 success: 1, // Успешная валидация
-                link: url
+                link: decodeURIComponent(url) // Возвращаем декодированный URL
             });
         } else {
             return res.status(400).json({ success: 0, message: 'Недоступная ссылка' });
